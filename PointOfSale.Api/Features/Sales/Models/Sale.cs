b628dcdfe232b710faa26908980846603407f2a7
Jozuan Lopez <jozuanlopez@gmail.com>
2023-03-04 21:17:00 -0600

    Se renombraron ciertas propiedades de la entidad Sale

--- PointOfSale.Api/Features/Sales/Models/Sale.cs
+++ PointOfSale.Api/Features/Sales/Models/Sale.cs
@@ -10,23 +10,12 @@ public class Sale
 {
     [Key]
     public int Id { get; set; }
-    
-    [Column("customer_id")]
     public int CustomerId { get; set; }
     public virtual People Customer { get; set; } = null!;
-    
-    [Column("user_id")]
     public int UserId { get; set; }
     public virtual User User { get; set; } = null!;
-    
-    [Column("receipt_type")]
-    public int TipoComprobante { get; set; }
-
-    [Column("fecha_hora")]
-    public DateTime Date { get; set; }
-
-    [Column("ammount")]
+    public int ReceiptType { get; set; }
+    public DateTime DateTime { get; set; }
     public decimal Total { get; set; }
-    
     public int Status { get; set; }
 }
