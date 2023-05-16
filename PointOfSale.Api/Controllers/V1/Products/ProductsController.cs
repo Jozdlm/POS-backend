b8e48c8a18c2d537fd762d48dfe574843102bce7
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-15 20:44:18 -0600

    El codigo ha sidor formateado

--- PointOfSale.Api/Controllers/V1/Products/ProductsController.cs
+++ PointOfSale.Api/Controllers/V1/Products/ProductsController.cs
@@ -97,26 +97,27 @@ public class ProductsController : ControllerBase
             );
         }
 
-        return Ok("Producto actualizado correctamente");
+        return Ok(new { message = "Producto actualizado correctamente" });
     }
 
     [HttpDelete("{id:int}")]
-    public async Task<ActionResult> DeleteProduct(int id) { 
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
         var alreadyExists = await _repository.AlreadyExists(id);
 
-        if(!alreadyExists)
+        if (!alreadyExists)
         {
             return NotFound();
         }
 
         var result = await _repository.Delete(id);
 
-        if(result == 0)
+        if (result == 0)
         {
             return BadRequest();
         }
 
-        return Ok(new {message = "Producto eliminado correctamente"});
+        return Ok(new { message = "Producto eliminado correctamente" });
     }
 
     [HttpGet("kardex")]
