c310d402a0b0fa517a31dab549e2fd78c0499b42
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-17 18:54:06 -0600

    Se ha movido de carpeta la entidad PurchaseItem

---
+++ PointOfSale.Api/Domain/Entities/PurchaseItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using PointOfSale.Api.Features.Products.Models;
+using PointOfSale.Api.Models;
+
+namespace PointOfSale.Api.Features.Purchases.Models;
+
+[Table("purchase_items")]
+public class PurchaseItem
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public int PurchaseId { get; set; }
+    public virtual Purchase Purchase { get; set; } = null!;
+    
+    public int ProductId { get; set; }
+    public virtual Product Product { get; set; } = null!;
+    
+    public string Batch {get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public int CurrentStock { get; set; }
+    public decimal PurchasePrice { get; set; }
+
+}
