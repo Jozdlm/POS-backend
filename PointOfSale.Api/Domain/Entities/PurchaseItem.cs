a734b6a05c554fb57c2f8ac880707d5df30cae2d
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-17 18:54:51 -0600

    Se ha formateado el código de la entidad PurchaseItem

--- PointOfSale.Api/Domain/Entities/PurchaseItem.cs
+++ PointOfSale.Api/Domain/Entities/PurchaseItem.cs
@@ -10,16 +10,15 @@ public class PurchaseItem
 {
     [Key]
     public int Id { get; set; }
-    
+
     public int PurchaseId { get; set; }
     public virtual Purchase Purchase { get; set; } = null!;
-    
+
     public int ProductId { get; set; }
     public virtual Product Product { get; set; } = null!;
-    
-    public string Batch {get; set; } = string.Empty;
+
+    public string Batch { get; set; } = string.Empty;
     public int Quantity { get; set; }
     public int CurrentStock { get; set; }
     public decimal PurchasePrice { get; set; }
-
 }
