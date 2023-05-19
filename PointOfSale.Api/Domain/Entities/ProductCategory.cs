d9a5df82f5764dfe93a5e2ad2df51603a0f27ffd
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-18 22:30:34 -0600

    Se movido la entidad ProductCategory a la capa Domain del proyecto

--- PointOfSale.Api/Domain/Entities/ProductCategory.cs
+++ PointOfSale.Api/Domain/Entities/ProductCategory.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PointOfSale.Api.Features.Products.Models;
+namespace PointOfSale.Api.Domain.Entities;
 
 [Table("product_categories")]
 public class ProductCategory
