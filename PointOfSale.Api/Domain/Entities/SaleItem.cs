3f10a500992e249f5119ab4b463c4a923f32497b
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-18 21:45:32 -0600

    Se ha corregido el namespace de la entidad SaleItem

--- PointOfSale.Api/Domain/Entities/SaleItem.cs
+++ PointOfSale.Api/Domain/Entities/SaleItem.cs
@@ -1,21 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using PointOfSale.Api.Features.Products.Models;
-using PointOfSale.Api.Models;
 
-namespace PointOfSale.Api.Features.Sales.Models;
+namespace PointOfSale.Api.Domain.Entities;
 
 [Table("sale_items")]
 public class SaleItem
 {
     [Key]
     public int Id { get; set; }
-    
+
     public int SaleId { get; set; }
 
     public int ProductId { get; set; }
     public virtual Product Product { get; set; } = null!;
-    
+
     public int Quantity { get; set; }
     public decimal PurchasePrice { get; set; }
     public decimal SellingPrice { get; set; }
