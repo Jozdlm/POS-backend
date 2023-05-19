27b6244bcae283d687e3b32d6927b3924fa388f5
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-18 22:40:16 -0600

    Definidos los map del ProductDto y el ProductResponse

--- PointOfSale.Api/Application/Mappings/ProductMappings.cs
+++ PointOfSale.Api/Application/Mappings/ProductMappings.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using PointOfSale.Api.Application.Contracts;
 using PointOfSale.Api.Domain.Entities;
 using PointOfSale.Api.Features.Products.Contracts;
+using PointOfSale.Api.Features.Products.Models;
 
 namespace PointOfSale.Api.Application.Mappings;
 
@@ -9,8 +10,27 @@ public class ProductMappings : Profile
 {
     public ProductMappings()
     {
+        ProductMap();
         ProductKardexMap();
         ProductCategoriesMap();
+    }
+
+    public void ProductMap()
+    {
+        CreateMap<ProductDto, Product>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.product_name))
+            .ForMember(dest => dest.ImgUrl, opt => opt.MapFrom(src => src.img_url))
+            .ForMember(dest => dest.MinStock, opt => opt.MapFrom(src => src.min_stock))
+            .ForMember(dest => dest.SellingPrice, opt => opt.MapFrom(src => src.selling_price))
+            .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.category_id))
+            .ReverseMap();
+
+        CreateMap<Product, ProductResponse>()
+            .ForMember(dest => dest.product_name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.img_url, opt => opt.MapFrom(src => src.ImgUrl))
+            .ForMember(dest => dest.min_stock, opt => opt.MapFrom(src => src.MinStock))
+            .ForMember(dest => dest.selling_price, opt => opt.MapFrom(src => src.SellingPrice))
+            .ForMember(dest => dest.category, opt => opt.MapFrom(src => src.Category));
     }
 
     public void ProductKardexMap()
