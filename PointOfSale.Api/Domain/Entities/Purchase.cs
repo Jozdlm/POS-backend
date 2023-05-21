dd02ddd2c40372b6c1b508828c920838b94f3df2
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-20 21:55:46 -0600

    Renombradas las propiedades de la entidad Purchase

--- PointOfSale.Api/Domain/Entities/Purchase.cs
+++ PointOfSale.Api/Domain/Entities/Purchase.cs
@@ -12,17 +12,14 @@ public class Purchase
     public int Id { get; set; }
 
     public string DocumentNumber { get; set; } = string.Empty;
+    public int DocumentType { get; set; }
+    public DateTime DateTime { get; set; }
+    public decimal Ammount { get; set; }
+    public int Status { get; set; }
     
     public int SupplierId { get; set; }
     public virtual People Supplier { get; set; } = null!;
     
     public int UserId { get; set; }
     public virtual User User { get; set; } = null!;
-    
-    public string TipoComprobante { get; set; } = null!;
-    public string NumCompra { get; set; } = null!;
-    public DateTime FechaHora { get; set; }
-    public decimal Tax { get; set; }
-    public decimal TotalCompra { get; set; }
-    public string Estado { get; set; } = null!;
 }
