f8234b2785d8695d64b04286382364b878ac4e51
Jozuan Lopez <jozuanlopez@gmail.com>
2023-05-23 19:21:58 -0600

    Se ha movido la entidad User a la capa Domain del proyecto

--- PointOfSale.Api/Domain/Entities/User.cs
+++ PointOfSale.Api/Domain/Entities/User.cs
@@ -1,24 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PointOfSale.Api.Features.Users.Models;
+namespace PointOfSale.Api.Domain.Entities;
 
 [Table("users")]
 public class User
 {
     [Key]
     public int Id { get; set; }
-    
+
     [Required]
     public string Username { get; set; } = null!;
 
     [Index(IsUnique = true)]
     [Required]
     public string Email { get; set; } = null!;
-    
+
     public string Phone { get; set; } = string.Empty;
-    
-    [Column("img_url")]
     public string ImgUrl { get; set; } = string.Empty;
-    public bool Active { get; set; }
+    public bool IsActive { get; set; }
 }
